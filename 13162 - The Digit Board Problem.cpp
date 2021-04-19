64debb48d1796e58770c4c7e4eac3039e47a2aa8
JunJunOWO <57358144+JunJunOWO@users.noreply.github.com>
2021-04-19 14:20:44 +0800

    Add files via upload

---
+++ 13162 - The Digit Board Problem.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int numOfCases = 0;
+	cin >> numOfCases;
+	long long int* sumOfEachSlots;
+	for (int i = 0; i < numOfCases; i++) {
+		int numOfSlots = 0;
+		int numOfRounds = 0;
+		cin >> numOfSlots >> numOfRounds;
+		sumOfEachSlots = new long long int[numOfSlots];
+		for (int j = 0; j < numOfSlots; j++)sumOfEachSlots[j] = 0;
+		for (int j = 0; j < numOfRounds; j++) {
+			int slot = -1;
+			cin >> slot;
+			string s;
+			getline(cin, s);
+			stringstream ss;
+			ss.str(s);
+			int toBeInserted = 0;
+			while (ss >> toBeInserted) {
+				sumOfEachSlots[slot] += toBeInserted;
+			}
+		}
+		long long int sum = 0;
+		for (int j = 0; j < numOfSlots; j++) {
+			sum += sumOfEachSlots[j] * (int)pow(10, numOfSlots - j - 1);
+		}
+		cout << sum << endl;
+		delete[] sumOfEachSlots;
+	}
+}
